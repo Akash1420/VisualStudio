8bf0bc1a22f027af5f90220f7692569f04a80994
Sakshi <dhasmanasakshi@gmail.com>
2020-08-06 13:45:25 +0530

    commit
    Signed-off-by: Sakshi <dhasmanasakshi@gmail.com>

---
+++ src/app/app.component.ts
@@ -0,0 +1,21 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'Routing';
+  // constructor(private router:Router){}
+  // getHome(){
+  //   this.router.navigate(['/homeLink']);
+  // }
+  // getLogin(){
+  //   this.router.navigate(['/loginLink']);
+  // }
+  // getRegister(){
+  //   this.router.navigate(['/registerLink']);
+  // }
+  
+}
